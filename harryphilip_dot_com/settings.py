e32a3f3134bb3ade7506502a68938b76a816c64c
Harry Philip <mail@harryphilip.com>
2018-12-08 16:10:26 -0800

    Make Secret Key Secret
    Move secret settings to new section and generate new secret key.

--- harryphilip_dot_com/settings.py
+++ harryphilip_dot_com/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+from .settings_secret import *
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -19,8 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '0r@r@*_833#zm$9)zgaxr8!e*wgvy0tm@##_(luk4bp8*@sdw-'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
